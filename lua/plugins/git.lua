759514ca428c95ba6775bd6d496ecc760791bdf8
RafaelFernandes12 <rfsilva786@gmail.com>
2024-12-26 23:25:38 -0300

    log keymap added to neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,30 +1,31 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
 
-      "nvim-telescope/telescope.nvim",
-      "ibhagwan/fzf-lua",
-      "echasnovski/mini.pick",
-    },
-    config = function()
-      require("neogit").setup({})
-      local keymap = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
+			"nvim-telescope/telescope.nvim",
+			"ibhagwan/fzf-lua",
+			"echasnovski/mini.pick",
+		},
+		config = function()
+			require("neogit").setup({})
+			local keymap = vim.api.nvim_set_keymap
+			local opts = { noremap = true, silent = true }
 
-      keymap("n", "<leader>gg", ":Neogit<CR>", opts)
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true }),
-        vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<CR>", { noremap = true, silent = true }),
-        vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { noremap = true, silent = true }),
-      })
-    end,
-  },
+			keymap("n", "<leader>gg", ":Neogit<CR>", opts)
+			keymap("n", "<leader>gl", ":Neogit log<CR>", opts)
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true }),
+				vim.keymap.set("n", "<leader>gR", ":Gitsigns reset_buffer<CR>", { noremap = true, silent = true }),
+				vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { noremap = true, silent = true }),
+			})
+		end,
+	},
 }
