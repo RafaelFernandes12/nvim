4148038fb6147d5a8372e2fa828275e39f35f7f7
RafaelFernandes12 <rfsilva786@gmail.com>
2025-01-20 14:56:36 -0300

    color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,44 +1,67 @@
+local custom_highlights = {
+	Normal = { bg = "#000000" }, -- Main buffer
+	NormalNC = { bg = "#000000" }, -- Non-current buffer
+	NormalFloat = { bg = "#000000" }, -- Floating windows
+	FloatBorder = { bg = "#000000" }, -- Floating window borders
+	SignColumn = { bg = "#000000" }, -- Sign column (e.g., diagnostics)
+	LineNr = { bg = "#000000" }, -- Line numbers
+	EndOfBuffer = { bg = "#000000" }, -- Background after the end of buffer
+	WinSeparator = { bg = "#000000" }, -- Window separators
+}
 return {
-  {
-    -- "folke/tokyonight.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    -- 	vim.cmd.colorscheme("tokyonight-night")
-    -- end,
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
-  {
-    -- "rose-pine/neovim",
-    -- name = "rose-pine",
-    -- config = function()
-    -- 	vim.cmd("colorscheme rose-pine-moon")
-    -- end,
-  },
-  {
-    -- "bluz71/vim-moonfly-colors",
-    -- name = "moonfly",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    -- 	vim.cmd.colorscheme("moonfly")
-    -- end,
-  },
-  {
-    -- "ellisonleao/gruvbox.nvim",
-    -- priority = 1000,
-    -- config = function()
-    -- 	vim.o.background = "dark" -- or "light" for light mode
-    -- 	vim.cmd([[colorscheme gruvbox]])
-    -- end,
-    -- opts = ...,
-  },
+	{
+		-- "folke/tokyonight.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("tokyonight-night")
+		-- end,
+	},
+	{
+		-- "catppuccin/nvim",
+		-- lazy = false,
+		-- name = "catppuccin",
+		-- priority = 1000,
+		-- config = function()
+		-- 	require("catppuccin").setup({
+		-- 		flavour = "mocha", -- Choose the variant
+		-- 		custom_highlights = custom_highlights,
+		-- 	})
+		-- 	vim.cmd.colorscheme("catppuccin")
+		-- end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				variant = "moon",
+				-- custom_highlights = custom_highlights,
+				palette = {
+					moon = {
+						base = "#000000",
+					},
+				},
+			})
+			vim.cmd("colorscheme rose-pine-moon")
+		end,
+	},
+	{
+		-- "bluz71/vim-moonfly-colors",
+		-- name = "moonfly",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("moonfly")
+		-- end,
+	},
+	{
+		-- "ellisonleao/gruvbox.nvim",
+		-- priority = 1000,
+		-- config = function()
+		-- 	vim.o.background = "dark" -- or "light" for light mode
+		-- 	vim.cmd([[colorscheme gruvbox]])
+		-- end,
+		-- opts = ...,
+	},
 }
--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,20 +2,6 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
   config = function()
-    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-    -- capabilities.textDocument.foldingRange = {
-    --   dynamicRegistration = false,
-    --   lineFoldingOnly = true,
-    -- }
-    --
-    -- local lspconfig = require("lspconfig")
-    -- local language_servers = lspconfig.util.available_servers()
-    -- for _, ls in ipairs(language_servers) do
-    --   lspconfig[ls].setup({
-    --     capabilities = capabilities,
-    --   })
-    -- end
-    --
     vim.keymap.set("n", "zr", require("ufo").openAllFolds)
     vim.keymap.set("n", "zm", require("ufo").closeAllFolds)
 
